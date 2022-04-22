88e1e156f8d0cb057ddcd65a115d6e88b4f482e1
Yacine REZGUI <yacine.rezgui@gmail.com>
2022-04-22 12:41:58 +0100

    master

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * You can place your custom package configuration in here.
+ */
+return [
+    'Device_Port' => 'COM1',
+    'Device_BaudRate' => '2400',
+    'Device_Parity' => 'none',
+    'Device_CharacterLength' => '8',
+    'Device_StopBits' => '1',
+    'Device_FlowControl' => 'none',
+];
